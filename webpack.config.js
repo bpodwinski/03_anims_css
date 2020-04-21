ae4719f30af52527138d87238133c4fbbe285128
Benoit Podwinski <me@benoitpodwinski.com>
2020-04-21 11:16:28 +0200

    finish

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,48 @@
 const webpack = require('webpack');
 const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+let sourceMap = true;
 
 if (process.env.NODE_ENV === 'production') {
     sourceMap = false;
 }
 
 module.exports = {
+    mode: 'development',
     entry: {
         main: [
-            './sass/style.scss'
-        ]
+            './assets/sass/style.scss'
+        ],
     },
     output: {
         path: path.resolve(__dirname, './'),
         filename: 'webpack.log'
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: './assets/css/[name].css',
+            chunkFilename: './assets/css/[id].css'
+        }),
+    ],
     module: {
         rules: [
             {
-                test: /\.scss$/,
+                test: /\.(sa|sc|c)ss$/,
                 use: [
-                    { loader: MiniCssExtractPlugin.loader },
-                    { loader: "css-loader" },
-                    { loader: "sass-loader" }
-                ]
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '../',
+                        },
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'sass-loader'
+                    },
+                ],
             },
-        ]
+        ],
     },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "style.css"
-        }),
-    ]
 };
