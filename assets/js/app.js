990b10752fdf0be0123879c37ad200d66e949159
Benoit Podwinski <me@benoitpodwinski.com>
2020-05-01 11:42:01 +0200

    add sticky menu for mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,3 +11,21 @@ overlay.addEventListener('click', function overlay() {
     menu.classList.remove("active")
     this.classList.remove("active")
 })
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("mobile-menu-wrapper");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+} 
